b8182232f9b4fdea5d2fb97cb5cff71baec88d13
hcen <panzetaoyx@qq.com>
2019-03-06 21:25:50 +0800

    saveUser

--- models/user.js
+++ models/user.js
@@ -12,74 +12,44 @@ function User (user){
 
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
 
-  db.close();
-});
+//   db.close();
+// });
 
 
-module.exports = User;
+
 //存
-User.prototype.save = function(callback){
-    //data 
-    var user = {
-        name:this.name,
-        password:this.password,
-        email:this.email
-    };
-    // onpen db
-    MongoClient.connect(url,function(err,db){
-        asert.equal(null,err);
-        console.error(err);
-        db.collection('users',function(err,collection){
-            collection.insertOne(user,function(err,result){
-                db.close();
-                callback(result);
-            })
-        })
-        console.log('connecting seccessfuly to blog');
-        db.close;
-    })
-}
+
 //取
-var kuser = new User({
-    name:'admin',
-    password:'admin',
-    email:'123@hcen.xyz'
-});
-console.log('111');
-kuser.save;
-console.log('111');
+
 var insertDocuments = function(db, callback) {
+    var user = {
+      name: this.name,
+      password: this.password,
+      email: this.email
+    };
     // Get the documents collection
-    var collection = db.collection('documents');
+    var collection = db.collection('users');
     // Insert some documents
-    collection.insertMany([
-      {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
-      assert.equal(err, null);
-      assert.equal(3, result.result.n);
-      assert.equal(3, result.ops.length);
-      console.log("Inserted 3 documents into the collection");
+    collection.insertOne(user, function(err, result) {
       callback(result);
     });
   }
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-
-// Connection URL
-var url = 'mongodb://localhost:27017/blog';
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  insertDocuments(db, function() {
-    db.close();
+User.prototype.save = function(callback){
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+  
+    insertDocuments(db, function() {
+      db.close();
+    });
   });
-});
+}
+// Use connect method to connect to the server
+
 
 var findDocuments = function(db, callback) {
     // Get the documents collection
@@ -92,19 +62,17 @@ var findDocuments = function(db, callback) {
       callback(docs);
     });
   }
-  var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/blog';
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server");
 
-  insertDocuments(db, function() {
-    findDocuments(db, function() {
-      db.close();
-    });
-  });
-});
+//   insertDocuments(db, function() {
+//     findDocuments(db, function() {
+//       db.close();
+//     });
+//   });
+// });
+
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+var User = require('../models/user');
 
 /* GET home page. */
 
@@ -15,6 +17,28 @@ router.get('/post',function(req,res){
   res.render('post',{title : 'post'});
 });
 router.get('/reg',function(req,res){
+  // register
+  var name = req.body.name,
+    password = req.body.password,
+    email = req.body.email;
+  var md5 = crypto.createHash('md5');
+  var password = md5.update(req.body.password).digest('hex');
+  var newUser = new User({
+    name: req.body.name,
+    password: password,
+    email: req.body.email
+  });
+  //检查用户名是否已存在
+  //todo
+  
+  //新建用户
+  newUser.save(function (err,user){
+    if(err){
+      return res.redirect('/reg');
+    }
+    req.session.user = user;
+    redirect('/');
+  });
   res.render('reg',{title : 'register'});
 });
 
